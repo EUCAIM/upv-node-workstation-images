44993d0bba769b214094122e3b695a2dd8899dfd
palollo <palollo@i3m.upv.es>
2022-02-28 14:02:29 +0100

    Option to build jupyter image from the one with pytorch

--- build-test-push-jupyter.template.cmd
+++ build-test-push-jupyter.template.cmd
@@ -1,36 +1,41 @@
 setlocal
-set V=1.1
+set V=1.2
+
+set CUDA_VERSION=
 REM Uncomment for building with CUDA
-REM set TENSOR_IMAGE_TAG=-gpu
 REM set CUDA_VERSION=cuda10
 
+set AI_TOOL=tensorflow
+REM Uncomment for building with pytorch
+REM set AI_TOOL=pytorch
+
 REM =========================================== Building all the images ===========================================
-docker build -t chaimeleon-eu.i3m.upv.es:10443/chaimeleon-library/ubuntu_python_tensorflow_jupyter:%V%%CUDA_VERSION% ^
-             --build-arg TENSOR_IMAGE_TAG=%TENSOR_IMAGE_TAG% --build-arg CUDA_VERSION=%CUDA_VERSION% ^
-             ubuntu_python_tensorflow_jupyter
+docker build -t chaimeleon-eu.i3m.upv.es:10443/chaimeleon-library/ubuntu_python_%AI_TOOL%_jupyter:%V%%CUDA_VERSION% ^
+             --build-arg CUDA_VERSION=%CUDA_VERSION% --build-arg AI_TOOL=%AI_TOOL% ^
+             ubuntu_python_xxxxx_jupyter
 
 REM ======================================== Deploying a container to test ========================================
 pause
-docker run -d --rm -p 98888:8888 ^
+docker run -d --rm -p 9888:8888 ^
            -e PASSWORD="chaimeleon" ^
            -e URL_PATH=test-jupyter ^
            --name testing02 ^
-           chaimeleon-eu.i3m.upv.es:10443/chaimeleon-library/ubuntu_python_tensorflow_jupyter:%V2%%CUDA_VERSION%
+           chaimeleon-eu.i3m.upv.es:10443/chaimeleon-library/ubuntu_python_%AI_TOOL%_jupyter:%V%%CUDA_VERSION%
 
 pause
 docker logs testing02
-REM Test jupyter service: open in the browser the URL localhost:98888/test-jupyter/.
+REM Test jupyter service: open in the browser the URL localhost:9888/test-jupyter/.
 pause
 docker stop testing02
 
 REM ====================================== Uploading the images to registry ======================================
 pause
 docker login -u registryuser chaimeleon-eu.i3m.upv.es:10443
-docker push chaimeleon-eu.i3m.upv.es:10443/chaimeleon-library/ubuntu_python_tensorflow_jupyter:%V%%CUDA_VERSION%
+docker push chaimeleon-eu.i3m.upv.es:10443/chaimeleon-library/ubuntu_python_%AI_TOOL%_jupyter:%V%%CUDA_VERSION%
 docker logout chaimeleon-eu.i3m.upv.es:10443
 
 REM ====================================== Removing local images ======================================
 pause
-docker rmi chaimeleon-eu.i3m.upv.es:10443/chaimeleon-library/ubuntu_python_tensorflow_jupyter:%V%%CUDA_VERSION%
+docker rmi chaimeleon-eu.i3m.upv.es:10443/chaimeleon-library/ubuntu_python_%AI_TOOL%_jupyter:%V%%CUDA_VERSION%
 
            
