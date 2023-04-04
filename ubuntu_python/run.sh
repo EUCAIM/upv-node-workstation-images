9f56e5ec09c3dcd464c080cb445bcaf73df0bd62
palollo <palollo@i3m.upv.es>
2023-04-04 10:38:09 +0200

    Add user custom init script

--- ubuntu_python/run.sh
+++ ubuntu_python/run.sh
@@ -9,5 +9,12 @@ fi
 # The usual path for binaries of apps installed by the user
 export PATH=$PATH:/home/chaimeleon/.local/bin
 
+# Execute the user custom init script if exists
+if [ -f /home/chaimeleon/persistent-home/init.sh ]; then
+    echo "### Executing the user custom init script ###"
+    bash /home/chaimeleon/persistent-home/init.sh
+    echo "### End of user custom init script ###"
+fi
+
 echo "$@" >.runcmd
 source .runcmd
